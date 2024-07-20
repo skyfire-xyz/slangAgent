9f1f0b5a8458df7efc0c8a1bf89e03aee765a511
hieuta000 <69173373+hieuta000@users.noreply.github.com>
2024-07-20 14:10:03 -0700

    static return of slang agent, working with aida

--- app.py
+++ app.py
@@ -12,12 +12,34 @@ def hello_world():
 
 @app.route('/v1/receivers/slang-agent', methods=['POST'])
 def get_best_response():
-    try:
-        chat_request = request.json
-        return chat_request
-    except Exception as e:
-        logger.error('Error processing request', exc_info=e)
-        return jsonify({'error': 'Chat Error'}), 401
+    return jsonify(
+            {
+            "prompt": "pop off girly pop, you're slayin in that grwm",
+            "body": "I scored 3 responses. Yasss queen! Serving looks and confidence like it's nobody's business!",
+            "payment": {
+                "status": "SUCCESS",
+                "sourceName": "hieu",
+                "sourceAddress": "0xc0E24Eb8aF398E2d98699D386599708EE4aAEd84",
+                "destinationName": "SlangAgent",
+                "destinationAddress": "0xb90376944F0fe36Ed005ec20963dB0d04E24058d",
+                "amount": "7535",
+                "currency": "USDC",
+                "generatedDate": "2024-07-20T21:05:28.436Z"
+            },
+            "quote": [
+                {
+                "status": "SUCCESS",
+                "sourceName": "hieu",
+                "sourceAddress": "0xc0E24Eb8aF398E2d98699D386599708EE4aAEd84",
+                "destinationName": "SlangAgent",
+                "destinationAddress": "0xb90376944F0fe36Ed005ec20963dB0d04E24058d",
+                "amount": "7535",
+                "currency": "USDC",
+                "generatedDate": "2024-07-20T21:05:28.436Z"
+                }
+            ]
+            }
+        )
 
 if __name__ == '__main__':
     app.run(debug=True)
